8b64566c66f9c70dda2886e3b95d87cedf87a6ab
Ulrich Hornung <hornunguli@gmx.de>
2024-07-22 11:46:39 +0200

    remove bookmark comment

--- service/service.go
+++ service/service.go
@@ -67,7 +67,6 @@ func New(config *config.C, logger *logrus.Logger) (*Service, error) {
 		return nil, errors.New("must be using user device")
 	}
 
-	// BOOKMARK: stack is created already here. CAN WE RE_USE IT?
 	s.ipstack = stack.New(stack.Options{
 		NetworkProtocols:   []stack.NetworkProtocolFactory{ipv4.NewProtocol, ipv6.NewProtocol},
 		TransportProtocols: []stack.TransportProtocolFactory{tcp.NewProtocol, udp.NewProtocol, icmp.NewProtocol4, icmp.NewProtocol6},
